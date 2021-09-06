e0a153405bd277e1ea8cfc239c0922e541d168d4
SmokeIce593 <37118809+SmokeIce593@users.noreply.github.com>
2021-09-06 15:40:53 -0400

    Initial commit

---
+++ src/main/java/org/example/Name.java
@@ -0,0 +1,13 @@
+package org.example;
+import java.util.Scanner;
+
+class Name {
+    public static void main( String[] args ) {
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.print( "What is your name?" );
+        String name = input.next();
+        System.out.println("Hello, " + name + ", nice to meet you!");
+    }
+}
