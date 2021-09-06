fc8f1d305cb460b8ac48d1a41131610a9a076ac1
SmokeIce593 <37118809+SmokeIce593@users.noreply.github.com>
2021-09-06 16:36:09 -0400

    Test

--- src/main/java/org/example/Name.java
+++ src/main/java/org/example/Name.java
@@ -1,3 +1,8 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 1 Solution
+ *  Copyright 2021 first_name last_name
+ */
+
 package org.example;
 import java.util.Scanner;
 
@@ -7,7 +12,7 @@ class Name {
         Scanner input = new Scanner(System.in);
 
         System.out.print( "What is your name?" );
-        String name = input.next();
+        String name = input.nextLine();
 
         String greeting = "Hello, " + name + ", nice to meet you!";
         System.out.println(greeting);
